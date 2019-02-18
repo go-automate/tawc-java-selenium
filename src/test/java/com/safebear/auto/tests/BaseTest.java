09d46ca47d8199777046d83eea8687d884270efd
student <student@safebear.co.uk>
2019-02-18 17:05:33 +0000

    AfterTest commit

--- src/test/java/com/safebear/auto/tests/BaseTest.java
+++ src/test/java/com/safebear/auto/tests/BaseTest.java
@@ -2,6 +2,7 @@ package com.safebear.auto.tests;
 
 import com.safebear.auto.utils.Utils;
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
 public class BaseTest {
@@ -10,6 +11,19 @@ public class BaseTest {
     @BeforeTest
     public void  setUp(){
         driver = Utils.getDriver();
+    }
+
+    @AfterTest
+    public void tearDown(){
+
+        try {
+            Thread.sleep(Integer.parseInt(System.getProperty("sleep","2000")));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        driver.quit();
+
     }
 
 }
